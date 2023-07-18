529c0d88f328c7783b47a3344debc15a054f12eb
Courtney Osborne <c@nosborne.co>
2023-07-18 16:14:52 -0500

    cleaned up personal side and blog

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -17,11 +17,11 @@ export default function AboutPage() {
       <p className="my-5 text-neutral-800 dark:text-neutral-200">
       Courtney Osborne is the co-founder of Agreedment, a SaaS proptech company. 
       He is an internet entrepreneur, real estate entrepreneur, software programmer, and former American wide receiver. 
-      He specializes in developing full-stack systems and architectural design in software applications using React.js, Next.js, Node.js, Python, Redis, MongoDB, Postgres, and Docker.
+      He specializes in developing full-stack systems and architectural design in software applications using iOS, Swift, Objective-C, Next.js, Python, Redis, Postgres, and Docker.
       Osborne has over 10 years of experience in building software programs and 5 years as a real estate entrepreneur. This includes creating lead generation and acquisition strategies for residential properties.
-      Steven's Backstory: With over 15 years in the real estate and software industries, Steven's journey began as a college athlete playing wide receiver for the University of Nebraska-Lincoln. 
+      Courtney's Backstory: With over 15 years in the real estate and software industries, Courtney's journey began as a college athlete playing safety and defensive back for the University of Nebraska-Lincoln. 
       He was part of the Cornhusker football team that secured the Legends Division's berth in the championship game under Bo Pelini in 2012.
-      Education: Steven graduated from the University of Nebraska-Lincoln in 2013 with a degree in Economics.
+      Education: Courtney graduated from the University of Nebraska-Lincoln in 2012 with a degree in Ethnic Studies.
       </p>
     </section>
     </>
