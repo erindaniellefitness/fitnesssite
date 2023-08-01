fd35042811d2c238b00b684363b4fa1620fdc1f6
erindaniellefitness <140550090+erindaniellefitness@users.noreply.github.com>
2023-07-31 20:19:27 -0500

    Update schema.tsx

--- lib/schema.tsx
+++ lib/schema.tsx
@@ -3,13 +3,13 @@ export const aboutPage = {
    "@context": "https://schema.org",
    "@type": "AboutPage",
    "@id": "https://nosborne.co/about#AboutPage",
-   "mainEntityOfPage": "https://nosborne.co/",
+   "mainEntityOfPage": "https://www.erindaniellefitness.com/",
    "isPartOf": {
       "@type": "WebSite",
-      "name": "Courtney Osborne",
-      "@id": "https://nosborne.co/#website"
+      "name": "Erin Davis",
+      "@id": "https://www.erindaniellefitness.com/#website"
    },
-   "lastReviewed": "2023-05-31",
+   "lastReviewed": "2023-07-31",
    "audience": [
       {
          "@type": "Audience",
@@ -21,111 +21,51 @@ export const aboutPage = {
       },
       {
          "@type": "Audience",
-         "name": "real estate entrepreneur"
-      },
-      {
-         "@type": "Audience",
-         "name": "software programmer"
+         "name": "personal trainer"
       }
    ],
-   "name": "About Courtney Osborne",
+   "name": "About ERIN DANIELLE",
    "about": {
       "@type": "Person",
-      "name": "Courtney Osborne",
-      "description": "Courtney Osborne is the co-founder of Agreedment, a SaaS proptech company. He is an internet entrepreneur, real estate entrepreneur, software programmer, and former American defensive back. He specializes in developing mobile systems and architectural design in mobile and software applications using iOS, Swift, Objective-c, Python, Redis, MongoDB, Postgres, and Docker. Osborne has over 10 years of experience in building software programs and 5 years as a real estate entrepreneur. This includes creating lead generation and acquisition strategies for residential properties. Courtney′s Backstory: With over 15 years in the real estate and software industries, Courtney′s journey began as a college athlete playing defensive back for the University of Nebraska-Lincoln. He was part of the Cornhusker football team that secured the Legends Division′s berth in the championship game under Bo Pelini in 2012. Education: Courtney graduated from the University of Nebraska-Lincoln in 2012 with a degree in Ethnic Studies.",
-      "url": "https://nosborne.co/",
-      "@id": "https://nosborne.co/#person",
-      "mainEntityOfPage": "https://nosborne.co/",
+      "name": "Erin Davis",
+      "description": "Courtney Osborne is the co-founder of fitness trainer ",
+      "url": "https://www.erindaniellefitness.com/",
+      "@id": "https://www.erindaniellefitness.com/#person",
+      "mainEntityOfPage": "https://www.erindaniellefitness.com/",
       "affiliation": {
        "@type":"SportsTeam",
        "name": "Nebraska Cornhuskers"
        },
       "height":[
          {"@type": "QuantitativeValue",
-         "value": "6-3"
+         "value": "5-4"
          }
        ],
        "weight":[
           {"@type": "QuantitativeValue",
-              "value": "200"
+              "value": "110"
           }
        ],
       "sameAs": [
-       "https://www.wikidata.org/entity/Q118849453",
-       "https://posts.google.com/claim/?mid=/m/0h_fsqk",
-       "https://www.imdb.com/name/nm5245794/",
-       "https://www.sports-reference.com/cfb/players/courtney-osborne-1.html",
-       "https://www.linkedin.com/in/courtney-osborne-engineer",
-       "https://www.instagram.com/courtthetwin/",
-       "https://github.com/bornsky",
-       "https://www.youtube.com/channel/UC1aBAGCKWIrUe8mw4lLhXww",
-       "https://www.crunchbase.com/person/courtney-osborne-0044",
-       "https://wellfound.com/u/courtney-osborne-2",
-       "https://golden.com/profile/courtney-osborne",
-       "https://muckrack.com/courtney-osborne",
-       "https://247sports.com/player/courtney-osborne-11700/",
-       "https://www.espn.com/college-football/player/stats/_/id/381031/courtney-osborne",
-       "https://twitter.com/courtthetwin_"
       ],
       "subjectOf": [
-       "https://n.rivals.com/content/athletes/courtney-osborne-41198",
-       "https://www.espn.com/college-football/player/_/id/381031/courtney-osborne",
-       "https://bleacherreport.com/articles/1230394-nebraska-football-should-nebraska-schedule-games-in-texas-for-recruiting",
-       "https://www.huskermax.com/2012-nebraska-minnesota-football-photos",
-       "https://www.huskermax.com/game/2010-texas-am-football",
-       "https://omaha.com/sports/huskers/article_d1a31264-2742-5d1f-a505-ffe9a5c82efc.html",
-       "https://omaha.com/sports/huskers/football/photos-nebraska-oklahoma-in-the-2010-big-ten-championship-game/collection_a11121a6-1708-11ec-b50e-2754fc8722d3.html#23",
-       "https://omaha.com/sports/huskers/football/chatelain-what-if-the-2010-big-12-championship-had-gone-differently-for-nebraska/article_0e159fd8-1692-11ec-b68e-ef6f63e21c84.html",
-       "https://www.youtube.com/watch?v=WcwxTLp6fT0",
-       "https://www.si.com/more-sports/2008/01/22/ballhawk-safeties",
-       "https://247sports.com/Player/Courtney-Osborne-11700/high-school-15884",
-       "https://huskers.com/sports/football/roster/courtney-osborne/14937",
-       "https://omaha.com/sports/huskers/football/photos-nebraska-oklahoma-in-the-2010-big-ten-championship-game/collection_a11121a6-1708-11ec-b50e-2754fc8722d3.html#1",
-       "https://www.oklahoman.com/story/sports/columns/berry-tramel/2010/12/05/ou-report-card-bob-stoops-timeout-oklahomas-pass-rush-move-to-the-head-of-the-class/61195830007",
-       "https://big12sports.com/news/2008/2/6/1522202.aspx",
-       "https://www.dailynebraskan.com/sports/making-calls-on-helmet-to-helmet-hits-challenging-for-referees-and-coaches-alike/article_6e0d5354-674b-5eda-8476-f505855adb8a.html",
-       "https://www.foxsports.com/stories/college-football/nebraska-leaving-big-12-as-runner-ups",
-       "https://journalstar.com/gallery/husker-football/gallery-holiday-bowl---nebraska-vs-washington/collection_d63c8633-6d25-5c12-86c5-e3b7843879a7.html#11",
-       "https://static.huskers.com/custompages/pdf8/729166.pdf",
-       "https://www.nytimes.com/2010/09/19/sports/ncaafootball/19huskies.html",
-       "https://www.al.com/sports/2010/12/jake_locker_leads_huskies_to_1.html",
-       "https://www.mccookgazette.com/story/1524838.html",
-       "https://www.espn.com/college-football/news/story?id=5753107",
-       "https://www.toledoblade.com/Michigan/2018/09/20/michigan-vs-nebraska-game-preview-everything-you-need-to-know-before-kickoff/stories/20180921012",
-       "https://www.dallasnews.com/sports/2010/12/05/big-12-championship-notebook-sooners-qb-moves-up-chart",
-       "https://www.gettyimages.com/detail/news-photo/oklahomas-ryan-broyles-tries-to-stop-nebraskas-courtney-news-photo/107332070",
-       "https://www.dallasnews.com/sports/2010/12/02/carlton-nebraska-s-messy-big-12-divorce-nears-its-climactic-end",
-       "https://huskers.com/sports/football/roster/courtney-osborne/14937",
-       "https://www.columbiatribune.com/story/sports/college/football/2010/11/01/legality-hit-questioned/21425873007",
-       "https://247sports.com/Article/31-for-2011-Courtney-Osborne-31944",
-       "https://theathletic.com/2206464/2020/11/19/nebraska-texas-am-2010-madness-revisited",
       ],
       "additionalType": [
-        "https://www.wikidata.org/wiki/Q45916492",
-        "https://www.wikidata.org/wiki/Q7301138",
-        "https://www.wikidata.org/wiki/Q52228055",
-        "https://www.wikidata.org/wiki/Q5482740",
-        "https://www.wikidata.org/wiki/Q674953"
+
      ],
       "image": [],
       "alternateName": [
-         "Courtney Osborne",
-         "Court Osborne"
+         "Erin Davis",
+         "ERIN DANIELLE"
       ],
-      "email": "c@nosborne.co",
+      "email": "activityiskey.ed@gmail.com",
       "familyName": "Osborne",
       "additionalName": "twin",
-      "givenName": "Courtney",
+      "givenName": "Erin",
       "honorificSuffix": "BA (Hons)",
-      "hasOccupation": "https://www.wikidata.org/wiki/Q45916492",
-      "jobTitle": "co-founder",
-      "gender": "Male",
-      "birthDate": "1990",
-      "worksFor": {
-            "@type": "Corporation",
-            "name": "Agreedment",
-            "url": "https://Agreedment.com/about/"
-     },
+      "hasOccupation": "https://www.wikidata.org/wiki/Q762121",
+      "jobTitle": "Personal Trainer",
+      "gender": "Female",
       "alumniOf": [
          {
             "@type": "CollegeOrUniversity",
@@ -134,8 +74,8 @@ export const aboutPage = {
          },
          {
             "@type": "HighSchool",
-            "name": "South Garland High School",
-            "sameAs": "https://en.wikipedia.org/wiki/South_Garland_High_School"
+            "name": "Stony Point High School",
+            "sameAs": "https://en.wikipedia.org/wiki/Stony_Point_High_School"
          }
       ],
       "award": [],
@@ -145,28 +85,8 @@ export const aboutPage = {
             "name": "United States"
          }
       ],
-      "sibling": [
-         {
-            "@type": "Person",
-            "name": "Steven Osborne",
-            "givenName": "Steven",
-            "familyName": "Osborne",
-            "gender": "Male",
-            "birthDate": "1990-01-15",
-            "url": "https://sosborne.co"
-         }
-      ],
-      "parent": [
-         {
-            "@type": "Person",
-            "name": "Sandra Osborne",
-            "givenName": "Sandra",
-            "familyName": "Osborne",
-            "gender": "Female"
-         }
-      ],
 
-      "disambiguatingDescription": "Courtney Osborne: Co-founder of Agreedment a proptech company, software programmer, Real Estate Investor, and Former University of Nebraska-Lincoln College American football player."
+      "disambiguatingDescription": "Erin Davis: Fitness Trainer"
 
      }
 
